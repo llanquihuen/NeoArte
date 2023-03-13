f8c3657785980a8c78be44483677f96312cd11cb
Luis Ramirez <llanquihuen@gmail.com>
2023-03-12 21:39:14 -0300

    cambios para agregar mensaje js

--- JS/utils.js
+++ JS/utils.js
@@ -41,6 +41,74 @@
         }
     }
 
+
+
+    //------ Formulario de contacto
+    const form = document.getElementById("contact-form");
+    const botonSubmit = document.getElementById("button_submit");
+
+    let tamanio = '1.5rem'
+    const x = window.matchMedia("(max-width: 560px)")
+    if (x.matches) {
+        tamanio ='.9rem'
+    }else{
+        tamanio = '1.5rem'
+    }
+
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+        botonSubmit.innerHTML = "Enviando...";
+        botonSubmit.disabled = true;
+        botonSubmit.classList.add("loading");
+        const formData = new FormData(form);
+        console.log(Object.fromEntries(formData.entries()));
+        fetch('https://www.neoarte.cl/enviomail/send', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(Object.fromEntries(formData.entries()))
+        })
+            .then(response => {
+                if (response.ok) {
+                    botonSubmit.innerHTML = "Enviar";
+                    botonSubmit.disabled = false;
+                    botonSubmit.classList.remove("loading");
+                    Toastify({
+                        text: 'Mensaje enviado correctamente',
+                        duration: 3000,
+                        gravity: 'bottom',
+                        position: 'right',
+                        style: {
+                            background: "linear-gradient(to right, #00b09b, #96c93d)",
+                            fontWeight: '600',
+                            fontSize: tamanio,
+                        },
+                    }).showToast();
+                    form.reset();
+                } else {
+                    console.error('Error al enviar el mensaje');
+                    botonSubmit.innerHTML = "Enviar";
+                    botonSubmit.disabled = false;
+                    botonSubmit.classList.remove("loading");
+                    Toastify({
+                        text: 'Error al enviar el mensaje',
+                        duration: 3000,
+                        gravity: 'bottom',
+                        position: 'right',
+                        style: {
+                            background: "linear-gradient(to right, #ca0018, #4d220b)",
+                            fontWeight: '600',
+                            fontSize: tamanio,
+                        },
+                    }).showToast();
+                    form.reset();
+                    }
+
+            })
+            .catch(error => console.error(error));
+    })
+
     // window.addEventListener("resize",()=>{
     //     if (window.innerWidth < 500){
     //         for (let i = 0; i < images.length; i++) {
